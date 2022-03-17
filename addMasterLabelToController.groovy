5b351627ee94bc77e4a18409dceff6b058ae4dbc
Tim Hosey <thosey@cloudbees.com>
2022-03-17 09:30:45 -0700

    Added master label add script
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

---
+++ addMasterLabelToController.groovy
@@ -0,0 +1,18 @@
+/*
+  *** WARNING ***
+  It is not recommended to EVER use the built-in node (on-controller executor).
+  Always use an Agent!
+*/
+println "Adding 'master' label to Controller..."
+println "This is not recommended. Don't use on-controller executors!"
+def nodes = []
+jenkins.model.Jenkins.instance.computers.each { c ->
+  if (c.node.labelString.contains("built-in")) {
+    if (c.node.labelString.contains("master")) {
+      println "Controller already has this label. Skipping."
+    } else {
+      c.node.labelString = c.node.labelString + " master"
+      println "New string = " + c.node.labelString
+    }
+  }
+}  
